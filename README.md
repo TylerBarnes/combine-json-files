2c76690eaafafad956177fe9588d20c12ac4afc5
Tyler Barnes <tylerdbarnes@gmail.com>
2020-03-27 15:36:15 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+combine-json
+============
+
+combines json files from the current directory into one json file
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/combine-json.svg)](https://npmjs.org/package/combine-json)
+[![Downloads/week](https://img.shields.io/npm/dw/combine-json.svg)](https://npmjs.org/package/combine-json)
+[![License](https://img.shields.io/npm/l/combine-json.svg)](https://github.com/TylerBarnes/combine-json/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g combine-json
+$ combine-json COMMAND
+running command...
+$ combine-json (-v|--version|version)
+combine-json/0.0.0 darwin-x64 node-v10.16.3
+$ combine-json --help [COMMAND]
+USAGE
+  $ combine-json COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+
+<!-- commandsstop -->
---
+++ src/index.js
@@ -0,0 +1,42 @@
+const {Command, flags} = require('@oclif/command')
+const fs = require('fs-extra')
+
+class CombineJsonCommand extends Command {
+  async run() {
+    const cwdFiles = await fs.readdir(process.cwd())
+    const jsonFiles = cwdFiles.filter(fileName => fileName.includes(`.json`))
+
+    const jsonArray = []
+
+    for (const jsonFile of jsonFiles) {
+      const fileContents = await fs.readJSON(jsonFile)
+
+      if (Array.isArray(fileContents)) {
+        fileContents.forEach(item => jsonArray.push(item))
+      } else {
+        jsonArray.push(fileContents)
+      }
+
+      // setTimeout(() => {
+      //   process.exit()
+      // }, 1);
+    }
+
+    await fs.writeJSON('__all.json', jsonArray)
+  }
+}
+
+CombineJsonCommand.description = `Describe the command here
+...
+Extra documentation goes here
+`
+
+CombineJsonCommand.flags = {
+  // add --version flag to show CLI version
+  version: flags.version({char: 'v'}),
+  // add --help flag to show CLI version
+  help: flags.help({char: 'h'}),
+  name: flags.string({char: 'n', description: 'name to print'}),
+}
+
+module.exports = CombineJsonCommand

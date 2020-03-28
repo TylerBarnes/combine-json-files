d0f3a1d0a7be44d24df0d14fb0c5a040a7fde414
Tyler Barnes <tylerdbarnes@gmail.com>
2020-03-27 17:53:41 -0700

    rename package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const {Command, flags} = require('@oclif/command')
 const fs = require('fs-extra')
 
-class CombineJsonCommand extends Command {
+class CombineJsonFilesCommand extends Command {
   async run() {
     const cwdFiles = await fs.readdir(process.cwd())
     const jsonFiles = cwdFiles.filter(fileName => fileName.includes('.json'))
@@ -22,12 +22,12 @@ class CombineJsonCommand extends Command {
   }
 }
 
-CombineJsonCommand.description = `Describe the command here
+CombineJsonFilesCommand.description = `Describe the command here
 ...
 Extra documentation goes here
 `
 
-CombineJsonCommand.flags = {
+CombineJsonFilesCommand.flags = {
   // add --version flag to show CLI version
   version: flags.version({char: 'v'}),
   // add --help flag to show CLI version
@@ -35,4 +35,4 @@ CombineJsonCommand.flags = {
   name: flags.string({char: 'n', description: 'name to print'}),
 }
 
-module.exports = CombineJsonCommand
+module.exports = CombineJsonFilesCommand
